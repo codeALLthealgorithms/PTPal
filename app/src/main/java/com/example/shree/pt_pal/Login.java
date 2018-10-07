cad93ff45e2403a549ed39e510f54c6e059b5e9d
codeALLthealgorithms <14ssampath@gmail.com>
2018-10-07 14:45:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/shree/pt_pal/Login.java
@@ -0,0 +1,47 @@
+package com.example.shree.pt_pal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class Login extends AppCompatActivity
+{
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Name = (EditText)findViewById(R.id.userUsername);
+        Password = (EditText)findViewById(R.id.userPassword);
+        Login = (Button) findViewById(R.id.loginButton);
+
+        Login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String userUsername, String userPassword)
+    {
+        if((userUsername.equals("admin")) && (userPassword.equals("test")))
+        {
+            Intent intent = new Intent(Login.this, Exercises.class);
+            startActivity(intent);
+        }
+
+    }
+}
